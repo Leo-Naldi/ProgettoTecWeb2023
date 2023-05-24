28e00a3c707bf0d3c125232569734d34ac909ad2
15926524 <fssfay5470@gmail.com>
2023-05-24 04:53:20 +0200

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ProgettoTecWeb2023
 Progetto Tecnologie Web 2023 UNIBO
+
+2023-05-24 modified files:
+backend:
+- server.js
+- config/index.js
+- models/message.js
+- routes/image.js
+- services/imageServices.js
+- services/messageService.js
+
+frontend:
+- quasar.config.js
+- boot/google-maps.js
+- pages/pageHome.vue
+
+to use send geolocation, insert a google map api key in myapp/src/boot/google-maps.js
--- myapp/src/boot/google-maps.js
+++ myapp/src/boot/google-maps.js
@@ -3,9 +3,8 @@ import VueGoogleMaps from "@fawmi/vue-google-maps";
 
 export default boot(({ app }) => {
   app.use(VueGoogleMaps, {
-    // 🤿 Vue App. Please install Vue Google Maps
     load: {
-      key: "AIzaSyBipjpcdZZghHlp9Rsh7LEgyKYaLw8iC_o", // 🤿 I don't have a google key, so leave it blank for now
+      key: "", // every month have $200 free charge, pricing: https://mapsplatform.google.com/pricing/
     },
   });
 });
