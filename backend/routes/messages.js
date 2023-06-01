7971af0434f886bf9a98d7051c73b04e47c6b4e4
krld <42297764+15926524@users.noreply.github.com>
2023-06-01 12:02:01 +0200

    merge with F's brach

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -12,4 +12,32 @@ MessageRouter.get('/', passport.authenticate(['basicAuth', 'anonymous']), async 
     await Controller.handleRequest(req, res, MessageServices.getMessages);
 })
 
+//send posts
+MessageRouter.post('/:handle/messages', passport.authenticate('basicAuth', { session: false }),
+    async (req, res) => {
+        if (req.params.handle !== req.user.handle)
+            res.sendStatus(409)
+    
+        await Controller.handleRequest(req,res, MessageServices.postUserMessage);
+    }
+);
+
+
+//seach post of a user
+MessageRouter.get('/:handle/messages', passport.authenticate('basicAuth', { session: false }),
+    async (req, res) => {
+        if (req.params.handle !== req.user.handle)
+            res.sendStatus(401)
+
+        await Controller.handleRequest(req, res, MessageServices.getUserMessages);
+    });
+
+//delete post of a user
+MessageRouter.delete('/:handle/messages/:id', passport.authenticate('basicAuth', { session: false }),
+    async (req, res) => {
+        
+        await Controller.handleRequest(req, res, MessageServices.deleteMessage);
+    }
+);
+
 module.exports = MessageRouter;
