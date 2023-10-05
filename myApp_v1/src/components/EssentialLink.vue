203ce737e1b66613bc1fbb07a41a5d0220662f8a
15926524 <fssfay5470@gmail.com>
2023-10-06 00:04:43 +0200

    leftsidebar css

--- myApp_v1/src/components/EssentialLink.vue
+++ myApp_v1/src/components/EssentialLink.vue
@@ -1,26 +1,29 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    target="_blank"
-    :href="link"
-  >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
-      <q-icon :name="icon" />
+  <q-item class="my-hover text-capitalize text-h6" clickable tag="a" :to="link">
+    <q-item-section v-if="icon" avatar>
+      <q-badge v-if="icon == 'notifications'" class="my-badage" rounded :label="unread_cnt" />
+      <q-icon size="md" :name="icon"></q-icon>
+
     </q-item-section>
 
-    <q-item-section>
+    <q-item-section class="between-icon">
       <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>{{ caption }}</q-item-label>
     </q-item-section>
+    <q-menu style="border-radius: 12px" v-if="icon == 'more_horiz'">
+      <q-list>
+        <q-item v-for="(item, i) in moreList" :key="i" v-close-popup clickable class="flex items-center">
+          <q-icon :name="item.icon" class="q-mr-sm" />
+          <q-item-section @click="gotoPage(item)">{{
+            item.lable
+          }}</q-item-section>
+        </q-item>
+      </q-list>
+    </q-menu>
   </q-item>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent,reactive } from 'vue'
 
 export default defineComponent({
   name: 'EssentialLink',
@@ -29,21 +32,65 @@ export default defineComponent({
       type: String,
       required: true
     },
-
-    caption: {
+    link: {
       type: String,
       default: ''
     },
-
-    link: {
-      type: String,
-      default: '#'
-    },
-
     icon: {
       type: String,
       default: ''
+    },
+    unread_cnt: {
+      type: Number,
+      default: 0
+    }
+  },
+  setup() {
+    return {
+      moreList: reactive([
+        {
+          lable: "Topics",
+          icon: "assistant_photo",
+          address: "",
+        },
+        {
+          lable: "Twitter for Professionals",
+          icon: "rocket_launch",
+          address: "",
+        },
+        {
+          lable: "Analytics",
+          icon: "equalizer",
+          address: "",
+        },
+        {
+          lable: "Settings",
+          icon: "verified_user",
+          address: "Settings",
+        },
+      ]),
     }
   }
 })
 </script>
+
+<style lang="sass" scoped>
+.my-hover
+  border-radius: 99px
+
+.my-hover:hover
+  background-color: #1da1f2
+  color: #e8f5ff
+  box-sizing: border-box
+
+.between-icon
+  margin-left:0.2rem
+
+.my-badage
+  position: absolute
+  top: 0.3rem
+  left: 0.8rem
+  background-color: red
+
+
+</style>
--- myApp_v1/src/layouts/MainLayout.vue
+++ myApp_v1/src/layouts/MainLayout.vue
@@ -1,10 +1,10 @@
 <template>
   <q-layout view="lHr LpR fFf">
     <q-header elevated>
-      <q-toolbar>
+      <q-toolbar class="my-headerbar">
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title>{{ $q.screen.width }} 宽度 x {{ $q.screen.height }} 高度, {{ $q.screen.gt.xs && $q.screen.lt.md }},
+        <q-toolbar-title>{{ $q.screen.width }} x {{ $q.screen.height }}, {{ $q.screen.gt.xs && $q.screen.lt.md }}
         </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
@@ -15,16 +15,50 @@
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above :mini="miniState.value" @mouseover="miniState.value = false"
-      @mouseout="miniState.value = !($q.screen.gt.xs && $q.screen.lt.md) ? false : true" :width="280" :breakpoint="600" bordered class="bg-grey-3">
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+      @mouseout="miniState.value = !($q.screen.gt.xs && $q.screen.lt.md) ? false : true" :width="320" :breakpoint="600"
+      bordered>
+      <div class="flex flex-center cursor-pointer" @click="gotoPage()">
+        <img alt="Quasar logo" src="~assets/logo_48.png" class="my-logo">
+      </div>
+      <div class="my-nav">
+        <q-list>
+          <EssentialLink class="nav-padding" v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+        </q-list>
+        <q-btn push v-if="leftDrawerOpen == true && miniState.value === false" unelevated rounded label="new" size="18px"
+          class="q-px-xl q-py-xs q-my-md my-button">
+        </q-btn>
+        <q-btn v-else round icon="history_edu" class="q-my-md">
+        </q-btn>
+      </div>
 
-        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+      <q-list class="absolute-bottom my-info">
+
+        <q-item class="cursor-pointer flex justify-between items-center  ">
+          <q-avatar avatar>
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+          </q-avatar>
+          <q-item-section class="my-avatar">
+            <q-item-label class="text-weight-medium">{{ user ? JSON.parse(user).username : "Null" }}</q-item-label>
+            <q-item-label caption>@{{ user ? JSON.parse(user).handle : "Null" }}</q-item-label>
+          </q-item-section>
+          <q-item-section side top>
+            <q-item-label caption><q-btn flat round color="grey-5" icon="more_horiz" /></q-item-label>
+          </q-item-section>
+          <q-menu style="border-radius: 12px;" :offset="[-90, 0]">
+            <!-- <q-menu style="border-radius: 1rem;" anchor="top middle" self="top middle"> -->
+            <q-list>
+              <q-item clickable v-close-popup @click="logout">
+                <q-item-section>Logout from @{{ user ? JSON.parse(user).handle : "Null" }}</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-item>
       </q-list>
     </q-drawer>
 
     <q-drawer :breakpoint="600" :mini="miniStateR.value" @mouseover="miniStateR.value = false"
-      @mouseout="miniStateR.value = !($q.screen.gt.xs && $q.screen.lt.md) ? false : true" :width="280" show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      @mouseout="miniStateR.value = !($q.screen.gt.xs && $q.screen.lt.md) ? false : true" :width="350" show-if-above
+      v-model="rightDrawerOpen" side="right" bordered>
       <div class="col-9 q-gutter-md q-pa-md">
         <q-input placeholder="Search Qwitter" outlined rounded dense>
           <template v-slot:prepend>
@@ -44,49 +78,48 @@
 import { defineComponent, ref, onMounted, watch, computed, toRefs } from "vue";
 import EssentialLink from "components/EssentialLink.vue";
 import { useQuasar } from "quasar";
+import { LocalStorage } from "quasar";
 
 const linksList = [
   {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev",
+    title: "home",
+    icon: "home",
+    link: "/home",
   },
   {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework",
+    title: "Search",
+    icon: "search",
+    link: "/Search",
   },
   {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev",
+    title: "Notify",
+    icon: "notifications",
+    link: "/notifications",
   },
   {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev",
+    title: "Channel",
+    icon: "group",
+    link: "/mychannel",
   },
   {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev",
+    title: "Bookmark",
+    icon: "bookmark",
+    link: "/bookmark",
   },
   {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev",
+    title: "User",
+    icon: "person",
+    link: "/user/details/" + LocalStorage.getItem('handle'),
   },
+  /*   {
+      title: "Quasar",
+      caption: "Community Quasar projects",
+      icon: "favorite",
+      link: "https://awesome.quasar.dev",
+    }, */
   {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev",
+    title: "more",
+    icon: "more_horiz",
   },
 ];
 
@@ -97,10 +130,16 @@ export default defineComponent({
   components: {
     EssentialLink,
   },
+  data() {
+    return {
+      token: localStorage.getItem("token"),
+      user: LocalStorage.getItem("user"),
+    };
+  },
   setup() {
     const leftDrawerOpen = ref(false);
     const rightDrawerOpen = ref(false);
-    const $q= useQuasar();
+    const $q = useQuasar();
     const miniState = ref(computed(() => {
       return !($q.screen.gt.xs && $q.screen.lt.md) ? ref(false) : ref(true)
     }));
@@ -123,3 +162,41 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="sass" scoped>
+.my-headerbar
+  background-color: #ffffff
+  color:#000000
+
+.my-nav
+  margin-top: 4rem
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+  overflow: hidden
+  box-sizing: border-box
+
+.nav-padding
+  padding: 0.7rem
+
+.my-button
+  width: 200px
+  height:48px
+  background-color: #000000
+  color: #f0f8ff
+.my-button:hover
+  background-color: #1da1f2
+  color: #e8f5ff
+
+.my-avatar
+  margin-left: 0.8rem
+
+.my-logo
+  margin-top:3rem
+  margin-bottom: -2rem
+
+.my-info:hover
+  background-color: #e8f5ff
+  border-radius: 99px
+</style>
