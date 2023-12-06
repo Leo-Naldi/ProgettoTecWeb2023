19cb85e8d1d91d46f64f67b97110def2288698af
Leo-Naldi <lnaldi99@gmail.com>
2023-12-06 17:23:18 +0100

    channel content

--- moddashboard/scripts/channelsContent.js
+++ moddashboard/scripts/channelsContent.js
@@ -21,7 +21,7 @@ class ChannelsContent {
                 return d.editors?.map(h => `@${h}`).join(', ');
             } else if (header === 'Creator') {
                 return `@${d.creator}`
-            }
+            } 
 
             return d[header.toLowerCase()];
         }
@@ -29,8 +29,10 @@ class ChannelsContent {
         let after_row_select = () => {
             if (this.data_table?.selected_item) {
                 this.edit_button?.attr('disabled', false);
+                this.delete_button?.attr('disabled', false);
             } else {
                 this.edit_button?.attr('disabled', true);
+                this.delete_button?.attr('disabled', true);
             }
         }
 
@@ -48,51 +50,61 @@ class ChannelsContent {
 
         this.edit_button = null;
         this.search_button = null;
+        this.delete_button = null;
     }
 
     mount() {
 
         this.container.empty();
 
-        let modal_id = 'edit-channel-modal';
+        let edit_modal_id = 'edit-channel-modal';
 
-        let modal = this.#makeEditDescriptionModal(modal_id);
+        let modal = this.#makeEditDescriptionModal(edit_modal_id);
+
+        let new_channel_modal_id = 'create-channel-modal';
+        let new_channel_modal = this.#makeChannelCreationModal(new_channel_modal_id);
 
         let s = `
-            <form id="message-search>     
-                <div class="my-3 form-group">
-                    <label class="form-label" for="authorSearchInput">Name</label>
-                    <input name="name" type="text" class="form-control" placeholder="Author..." id="authorSearchInput"> 
+            <form id="message-search">     
+                <div class="my-3 row form-group">
+                    <div class="col-md-8">
+                        <input class="form-control" name="name" type="text" placeholder="Author..." id="authorSearchInput"> 
+                    </div>
+                    <div id="message-search-buttons" class="col-auto d-flex">
+                        <button type="submit" class="btn btn-primary" class="mx-1">Search</button>
+                    </div>
                 </div>
             </form>
         `
 
         let filters_form = $(s);
 
-        filters_form.append($('<div>', {
-            'class': 'col-md-2',
-        }).append($('<div>', {
-            'class': 'form-group'
-        }).append($('<button>', {
-            'type': 'submit',
-            'class': 'btn btn-primary my-2',
-            'text': 'Search',
-        }))));
-
         this.edit_button = $('<button>', {
             'type': 'button',
-            'class': 'btn btn-primary',
+            'class': 'btn btn-primary mx-2',
             'text': 'Edit',
             'disabled': true,
             'data-bs-toggle': 'modal',
-            'data-bs-target': '#' + modal_id,
+            'data-bs-target': '#' + edit_modal_id,
         })
 
-        filters_form.append($('<div>', {
-            'class': 'col-md-2',
-        }).append($('<div>', {
-            'class': 'form-group'
-        }).append(this.edit_button)));
+        let new_button = $('<button>', {
+            'type': 'button',
+            'class': 'btn btn-primary',
+            'text': 'Create',
+            'data-bs-toggle': 'modal',
+            'data-bs-target': '#' + new_channel_modal_id,
+        });
+
+        this.delete_button = $(`
+            <button class="btn btn-danger mx-1" disabled>
+                <i class="bi bi-trash"></i>
+            </button>
+        `)
+
+        filters_form.find('#message-search-buttons').append(this.edit_button);
+        filters_form.find('#message-search-buttons').append(new_button);
+        filters_form.find('#message-search-buttons').append(this.delete_button);
 
         this.filters = filters_form;
 
@@ -111,8 +123,24 @@ class ChannelsContent {
             dt.filter = query;
         })
 
+        this.delete_button.on('click', (event) => {
+            event.preventDefault();
+            authorizedRequest({
+                endpoint: `/channels/${channel.name}`,
+                method: 'delete',
+                body: body,
+            }).then(() => {
+                this.data_table.mount();
+            });
+
+            this.edit_button?.attr('disabled', true);
+            this.delete_button?.attr('disabled', true);
+
+        })
+
         this.container.append(this.filters);
         this.container.append(modal);
+        this.container.append(new_channel_modal);
         this.container.append(this.table_container);
         this.data_table.mount();
     }
@@ -135,9 +163,8 @@ class ChannelsContent {
 
 
         let form = $(`
-            <form id="edit-message-form">
+            <form id="edit-channel-form">
                 <div class="mb-3">
-                    <label class="form-label" for="positive-reactions">Description</label>
                     <input id="description" name="description" class="form-control" type="text" />
                 </div>
                 <div class="mt-1 d-flex flex-row-reverse">
@@ -182,6 +209,72 @@ class ChannelsContent {
             });
 
             channel_cont.edit_button?.attr('disabled', true);
+        })
+
+        return modal;
+    }
+
+    #makeChannelCreationModal(id) {
+
+        let modal_headers = [
+            $('<h1>', {
+                'text': 'New Official Channel',
+                'class': 'modal-title fs-5',
+                'id': 'modal-title'
+            }),
+            $('<button>', {
+                'type': 'button',
+                'class': 'btn-close',
+                'data-bs-dismiss': 'modal',
+                'aria-label': 'Close'
+            }),
+        ]
+
+
+        let form = $(`
+            <form id="create-channel-form">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Channel's Name</label>
+                    <input id="name" name="name" class="form-control" type="text" />
+                </div>
+                <div class="mb-3">
+                    <label for="description" class="form-label">Channel's Descrtiption</label>
+                    <input id="description" name="description" class="form-control" type="text" />
+                </div>
+                <div class="mt-1 d-flex flex-row-reverse">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </form>
+        `);
+
+        let onHide = event => {
+            form.trigger('reset');
+        };
+
+        let modal = makeModDashboardModal(id, modal_headers, form, ()=>{}, onHide);
+
+        let dt = this.data_table;
+
+        form.on('submit', function (event) {
+
+            event.preventDefault();
+            let body = $(this).serializeArray().reduce((acc, cur) => {
+                acc[cur.name] = cur.value;
+                return acc;
+            }, {});
+
+            let name = body.name;
+            delete body.name;
+            body.official = true;
+
+            authorizedRequest({
+                endpoint: `/channels/${name}`,
+                method: 'post',
+                body: body,
+            }).then(() => {
+                modal.modal('hide');
+                dt.mount();
+            });
         })
 
         return modal;
