c3bd072a010641b9a1940d5a416e1d7dc902095d
Leo-Naldi <lnaldi99@gmail.com>
2023-12-26 17:19:14 +0100

    Possible fix

--- backend/utils/deleteImage.js
+++ backend/utils/deleteImage.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
 async function deleteImage(id) {
+
+    let obj_id = mongoose.mongo.ObjectId(id)
+
     let gridfs_bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
         bucketName: 'images',
     })
 
-    await gridfs_bucket.delete(id);
+    await gridfs_bucket.delete(obj_id);
 }
 
 module.exports = deleteImage;
